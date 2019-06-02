7183d18b252c5142877b883f6c7bf7dd7c368401
tanisha2398 <tanishanegipro@gmail.com>
2019-06-02 16:59:52 +0530

    add mongoDB connection and insert document using mongodb

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,39 @@
+const MongoClient = require("mongodb").MongoClient;
+MongoClient.connect(
+  "mongodb://localhost:27017",
+  { useNewUrlParser: true },
+  (err, client) => {
+    if (err) {
+      return console.log("Unable to connect to mongo db server");
+    }
+    var db = client.db("TodoApp");
+    console.log("connected to MongoDB server");
+
+    // db.collection("Todos").insertOne(
+    //   {
+    //     text: "Something to do",
+    //     completed: false
+    //   },
+    //   (err, result) => {
+    //     if (err) {
+    //       return console.log("unable to inser todo", err);
+    //     }
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    //   }
+    // );
+    db.collection("Users").insertOne(
+      {
+        name: "Tanisha",
+        age: 21,
+        location: "Type-3/56-J"
+      },
+      (err, result) => {
+        if (err) {
+          return console.log("unable to add to User Collection");
+        }
+        console.log(JSON.stringify(result.ops, undefined, 2));
+      }
+    );
+    client.close();
+  }
+);
