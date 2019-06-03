5a9a17b965f69e9f510548f6c09a0b648f2f5e6b
tanisha2398 <tanishanegipro@gmail.com>
2019-06-04 00:36:44 +0530

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+const { ObjectID } = require("mongodb");
 
 var { mongoose } = require("./db/mongoose");
 var { Todo } = require("./models/todo");
@@ -31,6 +32,25 @@ app.get("/todos", (req, res) => {
     },
     e => {
       res.status(400).send(e);
+    }
+  );
+});
+
+app.get("/todos/:id", (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    // console.log('Invalid id');
+    return res.status(404).send();
+  }
+  Todo.findById(id).then(
+    todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.send({ todo });
+    },
+    e => {
+      res.status(400).send();
     }
   );
 });
