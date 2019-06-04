f460f74b354dff107f1bc15d62a4c1e7418236f6
tanisha2398 <tanishanegipro@gmail.com>
2019-06-05 02:29:47 +0530

    mongodab Atlas

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,20 @@
 var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/TodoApp", {
-  useNewUrlParser: true
-});
+mongoose
+  .connect(
+    "mongodb+srv://tanishanegipro:tanisha23@cluster0-y8fdu.mongodb.net/test?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useCreateIndex: true
+    }
+  )
+  .then(() => {
+    console.log("connect to DB");
+  })
+  .catch(err => {
+    console.log("error:", err.message);
+  });
 
 module.exports = {
   mongoose
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var { Todo } = require("./models/todo");
 var { User } = require("./models/user");
 
 var app = express();
+const port = process.env.PORT || 3005;
 
 app.use(bodyParser.json());
 
@@ -55,8 +56,8 @@ app.get("/todos/:id", (req, res) => {
   );
 });
 
-app.listen(3005, () => {
-  console.log("Started on port 3005");
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
 });
 
 module.exports = { app };
