f1eb2cf1aa21339911d85cef761a1af7b8ff821a
tanisha2398 <tanishanegipro@gmail.com>
2019-06-05 22:25:34 +0530

    modifying user module and validating email and password using validator package

--- server/models/user.js
+++ server/models/user.js
@@ -1,11 +1,34 @@
 var mongoose = require("mongoose");
+const validator = require("validator");
 var User = mongoose.model("User", {
   email: {
     type: String,
     trim: true,
     required: true,
-    minlength: 1
-  }
+    minlength: 1,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: "{VALUE} is not a valid email"
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  tokens: [
+    {
+      access: {
+        type: String,
+        required: true
+      },
+      token: {
+        type: String,
+        required: true
+      }
+    }
+  ]
 });
 
 module.exports = { User };
--- server/server.js
+++ server/server.js
@@ -116,6 +116,20 @@ app.patch("/todos/:id", (req, res) => {
     });
 });
 
+app.post("/users", (req, res) => {
+  var body = _.pick(req.body, ["email", "password"]);
+  var user = new User(body);
+
+  user.save().then(
+    user => {
+      res.send(user);
+    },
+    e => {
+      res.status(400).send(e);
+    }
+  );
+});
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
