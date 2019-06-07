f39916e4bf0272b88f4ef90ca5f8b36a2e2949ba
tanisha2398 <tanishanegipro@gmail.com>
2019-06-07 14:19:23 +0530

    add DELETE/users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -55,6 +55,17 @@ UserSchema.methods.generateAuthToken = function() {
   });
 };
 
+UserSchema.methods.removeToken = function(token) {
+  var user = this;
+  return user.update({
+    $pull: {
+      tokens: {
+        token: token
+      }
+    }
+  });
+};
+
 UserSchema.statics.findByToken = function(token) {
   var User = this;
   var decoded;
